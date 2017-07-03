8f75280b4e67902fedd67fd069ccdde74fedefe0
Nicola Pounder <nicola.pounder@assimila.eu>
2017-07-03 14:52:58 +0100

    Define x/y limits in destination projection (remove lat-lon)

--- reprojectCAMS.sh
+++ reprojectCAMS.sh
@@ -53,7 +53,6 @@ gdalwarp \
     -s_srs EPSG:4326 \
     -t_srs '+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs ' \
     -te $XMIN $YMIN $XMAX $YMAX \
-    -te_srs EPSG:4326 \
     -tr 463.312719959778804 -463.312716551443543 \
     -co "COMPRESS=LZW" -co "INTERLEAVE=BAND" -co "TILED=YES" \
     NETCDF:"$INPUTFILE":$PARAMETER $OUTPUTFILE
