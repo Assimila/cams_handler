19f90926e31caaa6266bddccbd6932752c4a6e68
Nicola Pounder <nicola.pounder@assimila.eu>
2017-07-03 14:54:06 +0100

    Get limits from example file and reproject CAMS

---
+++ reproject_cams.py
@@ -0,0 +1,54 @@
+import gdal
+import subprocess
+
+def input_filename(tile):
+    return  "atmosvar_fc.nc"
+
+def output_filename(tile, var):
+    fname = '{var}_{tile}_sin_500m.tif'.format(var=var, tile=tile)
+    return fname
+
+def get_tile_extent(MOD09band):
+    ds = gdal.Open(MOD09band)
+    (xmin, xres, xrot, ymax, yrot, yres) = ds.GetGeoTransform()
+    xsize = ds.RasterXSize
+    ysize = ds.RasterYSize
+    xmax = xmin + xsize * xres
+    ymin = ymax + ysize * yres
+    return  xmin, xmax, xres, ymin, ymax, yres
+
+def reproject(var, infname, outfname, xmin, xmax, xres, ymin, ymax, yres):
+    args = ['./reprojectCAMS.sh', '-i', infname, 
+            '-o', outfname, 
+            '-p', var, 
+            '--xmin', str(xmin), 
+            '--ymin', str(ymin), 
+            '--xmax', str(xmax), 
+            '--ymax', str(ymax)]
+    print args
+    subprocess.call(args)
+
+
+
+def reproject_cams(MOD09band, tile = 'h17v05'):
+    #Get extent and resolution of modis file
+    xmin, xmax, xres, ymin, ymax, yres = get_tile_extent(MOD09band)
+    # Loop through variables
+    vars = ['tcwv', 'gtco3', 'aod550', 'sf']
+    for var in vars:
+        #Get input file 
+        infname = input_filename(tile)
+        #create output filename
+        outfname = output_filename(tile, var)
+        #reproject image
+        reproject(var, infname, outfname, xmin, xmax, xres, ymin, ymax, yres)
+        print xmin, xmax, xres, ymin, ymax, yres, infname, outfname
+        
+
+
+def main():
+    modisband = 'HDF4_EOS:EOS_GRID:"/media/Data/modis/h17v05/MOD09GA.A2016009.h17v05.006.2016012053256.hdf":MODIS_Grid_500m_2D:sur_refl_b02_1'
+    reproject_cams(modisband)
+
+if __name__ == "__main__":
+    main()
