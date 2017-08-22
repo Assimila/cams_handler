184bfa3e67760cf300076dde28b37343694fac1b
Nicola Pounder <nicola.pounder@assimila.eu>
2017-08-22 12:21:48 +0100

    add uncertainty

--- read_cams.py
+++ read_cams.py
@@ -5,8 +5,6 @@ import CAMS_utils
 
 gdal.UseExceptions()
 
-def filename(tile, date, var):
-    return CAMS_utils.vrt_filename(tile, date, var)
 
 def scaled_data(band):
     ''' read data array from band an apply the scale and offset
@@ -20,7 +18,8 @@ def scaled_data(band):
     offset = float(metadata['add_offset'])
     return (band.ReadAsArray() * scale + offset)
     return data
-    
+
+
 def convert_units(data, var):
     if var == 'tcwv':
         # source in kg m-2, want in g cm-2
@@ -31,6 +30,7 @@ def convert_units(data, var):
     # All other variables returned unchanged.
     # AOD550 is unitless. sf is in m of water equivilent
     return data
+
 
 def convertdatetime(netcdftime):
     ''' Convert the netcdf time from hours since 1900-01-01 00:00:0.0
@@ -44,17 +44,19 @@ def convertdatetime(netcdftime):
     hours = dt.timedelta(hours=netcdftime)
     return origin + hours
 
+
 def date_to_netcdf_time(datetime):
-    ''' Convert a datetime object to the netcdf time in hours since 1900-01-01 00:00:0.0
+    """ Convert a datetime object to the netcdf time in hours since 1900-01-01 00:00:0.0
     input:
         date and time in datetime object
     output:
         netcdftime date and time in hours since  1900-01-01 00:00:0.0
-    '''
+    """
     origin = dt.datetime(1900,1,1,0,0,0) #netcdf times are hours since this time
     timedelta = datetime - origin
     hours = timedelta.total_seconds()/3600
     return hours
+
 
 def get_timestamps(ds):
     ##############Not using this at present...
@@ -65,14 +67,28 @@ def get_timestamps(ds):
     datetimes = [convertdatetime(int(t)) for t in times]
     return datetimes
 
+
 def get_banddata(ds, step, var):
     band = ds.GetRasterBand(step)
     banddata = scaled_data(band)
     return convert_units(banddata, var)
 
+
+def uncertainty(data, var):
+    """ return uncertainty. Currently just 10% of exisitng value.
+    Input:
+        data: array of data
+        var: (string) variable name
+    Output
+    """
+    if var == "sf":
+        return 0
+    else:
+        return data*0.1
+
+
 def get_var(fname, date, var):
     ds = gdal.Open(fname)
-
     # Get right timestep
     # Get timestamps from metadata
     times = ds.GetMetadataItem('NETCDF_DIM_time_VALUES')
@@ -85,57 +101,39 @@ def get_var(fname, date, var):
     #get data
     data = get_banddata(ds, nrst_index, var)
     #Get uncertanties
-    return data, data_time
+    unc = uncertainty(data, var)
+    return data, unc, data_time
 
-'''
-    #loop through bands?
-    data = np.zeros((ds.RasterCount, ds.RasterXSize, ds.RasterYSize))
-    RasterCount = ds.RasterCount
-    ds = None
-    for i in xrange(RasterCount):
-        ds = gdal.Open(fname)
-        step = i+1 #gdal get raster band counts from 1, numpy from 0.
-        data[i, ...] = get_banddata(ds, step, var)
-        # read data
-        #band = ds.GetRasterBand(step)
-        #Data scale and offset
-        #banddata = scaled_data(band)
-        #band = None
-        #convert units
-        #data[i,...] = convert_units(banddata, var)
-        #banddata = None
-        datetimes = get_timestamps(ds)
-        ds = None   # Close dataset to free up memory.
-    return data, datetimes
-'''
 
 def read_cams(date, tile, directory):
-    '''
+    """
     Input:
         date - a datetime object with year, month, day, hours, minutes, seconds
         tile - modis tile reference eg h17v05
     Output:
-
-    '''
+        data: a dictionary of var:data pairs. Data is a 2D array.
+        unc: a dictionary of var:unc paris. Uncertainty is the uncertainty in data.
+        times: a dictionary of var:time pairs. time is a datetime object with the date and time of the
+               returned data.
+    """
     # Open file
     vars = CAMS_utils.parameters()
     data = {}
+    unc = {}
     times = {}
     for var in vars:
         print var
         fname = CAMS_utils.vrt_filename(tile, date.year, date.month, var, directory) #need to make the file/date thing work...
-        data[var], times[var] = get_var(fname, date, var)
-    return data, times
+        data[var], times[var], unc[var] = get_var(fname, date, var)
+    return data, unc, times
 
 
 def main():
-    #startdate = dt.date(2016,1,1)
-    #enddate = dt.date(2016, 3, 31)
     datetime = dt.datetime(2016, 2, 15, 10, 0, 0)
     tile = 'h17v05'
     directory = "data"
-    #return read_cams(startdate, enddate, tile)
     return read_cams(datetime, tile, directory)
+
 
 if __name__ == "__main__":
     main()
