08bcf15c60a63e9eccb36579cdc999b7089365ba
Nicola Pounder <nicola.pounder@assimila.eu>
2017-10-05 15:58:07 +0100

    add setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='cams_handler',
+    version ='0.1.0',
+    author='N Pounder',
+    url='https://github.com/Assimila/cams_handler',
+    packages=['cams_handler']
+)
